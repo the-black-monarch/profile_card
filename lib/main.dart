37b6a2d5361d2c4a5f3edd7a1cbc3da2c0be4979
Star Wolf Emperor <folorunshoyusuf8@gmail.com>
2023-01-31 07:52:39 +0100

    first project changes, added a profile image, name, job title and contact cards

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.black87,
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+                    CircleAvatar(
+            radius: 50.0,
+            backgroundImage: 
+            AssetImage('images/starwolf.png'),
+            backgroundColor: Colors.white,
+          ),
+          Text(
+            'StarWolf',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Pacifico',
+              fontWeight: FontWeight.bold,
+              fontSize: 40.0,
+            ),
+          ),
+          Text(
+            'SOFTWARE DEVELOPER',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Source Sans Pro',
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+            ),
+          ),
+          Card(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: ListTile(
+                leading: Icon(
+                  Icons.facebook,
+                  color: Colors.black87,
+                ),
+                title: Text(
+                  'facebook',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          Card(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: ListTile(
+                leading: Icon(
+                  Icons.discord,
+                  color: Colors.black87,
+                ),
+                title: Text(
+                  'discord',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          Card(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.black87,
+                ),
+                title: Text(
+                  'contact',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+        ],
+      )),
+    ));
+  }
+}
