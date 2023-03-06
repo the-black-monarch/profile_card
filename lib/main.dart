952abf3f249ce45945f3811825debfecda32aca5
Star Wolf Emperor <folorunshoyusuf8@gmail.com>
2023-03-06 01:32:15 +0100

    Added Comments to each widegets

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,15 @@ class MyApp extends StatelessWidget {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-                    CircleAvatar(
+          //CREATE AIRCLE AVATAR FOR PROFILE IMAGE
+          CircleAvatar(
             radius: 50.0,
-            backgroundImage: 
-            AssetImage('images/starwolf.png'),
+            backgroundImage: AssetImage('images/starwolf.png'),
             backgroundColor: Colors.white,
           ),
+          // USERNAME
           Text(
-            'StarWolf',
+            'Star Wolf',
             style: TextStyle(
               color: Colors.white,
               fontFamily: 'Pacifico',
@@ -31,6 +32,7 @@ class MyApp extends StatelessWidget {
               fontSize: 40.0,
             ),
           ),
+          //USER JOB TITLE
           Text(
             'SOFTWARE DEVELOPER',
             style: TextStyle(
@@ -40,57 +42,58 @@ class MyApp extends StatelessWidget {
               fontSize: 30.0,
             ),
           ),
+          //SOCIAL LINKS
           Card(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: ListTile(
-                leading: Icon(
-                  Icons.facebook,
-                  color: Colors.black87,
-                ),
-                title: Text(
-                  'facebook',
-                  style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20.0,
-                    color: Colors.black,
-                  ),
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: ListTile(
+              leading: Icon(
+                Icons.facebook,
+                color: Colors.black87,
+              ),
+              title: Text(
+                'facebook',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.black,
                 ),
               ),
             ),
+          ),
           Card(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: ListTile(
-                leading: Icon(
-                  Icons.discord,
-                  color: Colors.black87,
-                ),
-                title: Text(
-                  'discord',
-                  style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20.0,
-                    color: Colors.black,
-                  ),
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: ListTile(
+              leading: Icon(
+                Icons.discord,
+                color: Colors.black87,
+              ),
+              title: Text(
+                'discord',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.black,
                 ),
               ),
             ),
+          ),
           Card(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.black87,
-                ),
-                title: Text(
-                  'contact',
-                  style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20.0,
-                    color: Colors.black,
-                  ),
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: ListTile(
+              leading: Icon(
+                Icons.phone,
+                color: Colors.black87,
+              ),
+              title: Text(
+                'contact',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.black,
                 ),
               ),
             ),
+          ),
         ],
       )),
     ));
