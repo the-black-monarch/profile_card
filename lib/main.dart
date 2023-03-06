a54a23a3d861419427d6d4f5c3eb04a3fb8eb97d
Star Wolf Emperor <folorunshoyusuf8@gmail.com>
2023-03-06 07:20:07 +0100

    centered all widgets and cleaned up the code

--- lib/main.dart
+++ lib/main.dart
@@ -10,41 +10,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.black87,
-      body: SafeArea(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          //CREATE AIRCLE AVATAR FOR PROFILE IMAGE
-          CircleAvatar(
-            radius: 50.0,
-            backgroundImage: AssetImage('images/starwolf.png'),
-            backgroundColor: Colors.white,
-          ),
-          // USERNAME
-          Text(
-            'Star Wolf',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'Pacifico',
-              fontWeight: FontWeight.bold,
-              fontSize: 40.0,
+      home: Scaffold(
+        backgroundColor: Colors.black87,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //CREATE AIRCLE AVATAR FOR PROFILE IMAGE
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/starwolf.png'),
+                  backgroundColor: Colors.white,
+                ),
+                // USERNAME
+                Text(
+                  'Star Wolf',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Pacifico',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                  ),
+                ),
+                //USER JOB TITLE
+                Text(
+                  'SOFTWARE DEVELOPER',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Source Sans Pro',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  ),
+                ),
+                //SOCIAL LINKS
+              ],
             ),
           ),
-          //USER JOB TITLE
-          Text(
-            'SOFTWARE DEVELOPER',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'Source Sans Pro',
-              fontWeight: FontWeight.bold,
-              fontSize: 30.0,
-            ),
-          ),
-          //SOCIAL LINKS
-        ],
-      )),
-    ));
+        ),
+      ),
+    );
   }
 }
