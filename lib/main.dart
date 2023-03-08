1d10cc6fce218e1ae6afee1a146c71fda5d1ba34
Star Wolf Emperor <folorunshoyusuf8@gmail.com>
2023-03-08 07:13:33 +0100

    1. moved all widgets to a Stateful Widget. 2. added font_awesome icons package

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  return runApp(
+    MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black87,
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/starwolf.png'),
-                ),
-                Text(
-                  'Star Wolf',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'FLUTTER DEVELOPER',
-                  style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                  width: 150.0,
-                  child: Divider(
-                    color: Colors.white70,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+        body: Profile(),
       ),
-    );
-  }
+    ),
+  );
 }
 
 class Profile extends StatefulWidget {
@@ -66,12 +22,73 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          
-        ],
+    return SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/starwolf.png'),
+            ),
+            Text(
+              'Star Wolf',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.white,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.white70,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.facebook),
+                  label: Text('facebook'),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(FontAwesomeIcons.instagram),
+                  label: Text('Instagram'),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    (FontAwesomeIcons.twitter),
+                  ),
+                  label: Text('Twitter'),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    (FontAwesomeIcons.linkedin),
+                  ),
+                  label: Text('Linkedin'),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
-    ),
+    );
   }
 }
